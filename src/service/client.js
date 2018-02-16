a398c7b34e8b7b7ba8a9b5ce97422ebefd67373d
patricia.neri <patriciageneri@gmail.com>
2018-02-16 11:41:46 -0200

    Added token

--- src/service/client.js
+++ src/service/client.js
@@ -1,8 +1,8 @@
 import {createClient} from 'contentful'
 
 const client = createClient({
-  space: '',
-  accessToken: ''
+  space: 'wtbs85dpl3u8',
+  accessToken: '28268300d869d18fb84f95a04f17967543898910e37df48749cc1f01db654886'
 })
 
 export default client
